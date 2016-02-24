24e66ec29a6ce1599c3b472bfd8991c0ba1c8002
Prelang Builder <builder@prelang.com>
2016-02-24 18:27:22 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Zgloszenia' to allow for create, update

--- app/admin/zgloszenia.rb
+++ app/admin/zgloszenia.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Zgloszenia do
+  permit_params :imie, :nazwisko, :miasto, :adres, :telefon, :email, :data_urodzenia, :wiadomosc, :zaliczka, :oplacony
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
